cc4ebc94508e26796229ce7bb8178e813c8b2f37
Yoony Ok <jyo1031@gmail.com>
2019-12-10 12:01:51 -0800

    slack

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          HELLO ASSESSMENT DAY ZEITTTTTT
+          SLACK MESSAGE
         </p>
         <a
           className="App-link"
