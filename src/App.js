e6b62036d88b66ab6bfea58f57cc824cecc55ef8
Yoony Ok <jyo1031@gmail.com>
2019-12-10 11:19:46 -0800

    ZEIT

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          HELLO ASSESSMENT DAY
+          HELLO ASSESSMENT DAY ZEITTTTTT
         </p>
         <a
           className="App-link"
